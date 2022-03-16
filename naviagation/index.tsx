9a2eb313f2f70db756bc7a0b02ec037b48ec5a86
NicksyRop <nicksonkipkorir25@gmail.com>
2022-03-16 22:44:42 +0300

    test

--- naviagation/index.tsx
+++ naviagation/index.tsx
@@ -2,21 +2,49 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
 import PlannerScreen from "../screens/PlannerScreen";
-const Stack = createNativeStackNavigator()
+import TestScreen from "../screens/TestScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+
 
 
 export default function Navigator() {
 
     return(
         <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Planner" component={PlannerScreen} />
-        </Stack.Navigator>
+           <NavScreens/>
+       
     </NavigationContainer>
 
 
     )
 
 
+}
+const Stack = createNativeStackNavigator()
+function NavScreens(){
+
+    return(
+        <Stack.Navigator >
+        <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown : false}}/>
+        <Stack.Screen name="Test" component={TestScreen} />
+       
+    </Stack.Navigator>
+
+    )
+}
+
+const BottomTab = createBottomTabNavigator()
+function BottomTabNavigator (){
+
+    return(
+
+      <BottomTab.Navigator initialRouteName="Home">
+          <BottomTab.Screen  name="Home" component={HomeScreen}/>
+          <BottomTab.Screen  name="Planner" component={PlannerScreen}/>
+      </BottomTab.Navigator>
+
+
+    )
 }
--- screens/TestScreen.tsx
+++ screens/TestScreen.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 
-export default function HomeScreen({navigation} :NativeStackHeaderProps) {
+export default function TestScreen({navigation} :NativeStackHeaderProps) {
   
   return (
     <View>
-      <Text>HomeScreen</Text>
-      <Button title='Go to Test' onPress={()=> navigation.push("Test")}/>
+      <Text>TestScreen</Text>
+      <Button title='Go to Home' onPress={()=> navigation.push("Root")}/>
     </View>
   )
 }
