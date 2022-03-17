efebd35256391cca1b122e6a1bebcea8b12fcfbf
NicksyRop <nicksonkipkorir25@gmail.com>
2022-03-17 19:19:21 +0300

    added a util function to fortmat time to seconds

--- components/WorkoutItem.tsx
+++ components/WorkoutItem.tsx
@@ -1,12 +1,13 @@
 import { View, Text ,StyleSheet } from 'react-native'
 import React from 'react'
 import { Workout } from '../types/data'
+import { formatSec } from '../utils/Time'
 
 export default function WorkoutItem({item} : {item : Workout}) {
   return (
     <View style={styles.container}>
        <Text style={styles.name}>{item.name}</Text>
-       <Text style={styles.duration}>Duration :{item.duration}</Text>
+       <Text style={styles.duration}>Duration :{formatSec (item.duration)} min</Text>
         <Text style={styles.difficulty}>Difficulty :{item.difficulty}</Text>
     </View>
   )
